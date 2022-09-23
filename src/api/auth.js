99b57b8d9508deb48604f3460ed2a168d36c369b
vicentepz94 <vicentepz94@gmail.com>
2022-09-23 20:01:40 -0300

    Función para refrescar Access Token desde Client

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import { basePath, apiVersion } from "./config";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../utils/constants";
 import jwtDecode from "jwt-decode";
 
-export function getAccessToken() {
+export function getAccessTokenApi() {
   const accessToken = localStorage.getItem(ACCESS_TOKEN);
 
   if (!accessToken || accessToken === "null") {
@@ -12,13 +12,43 @@ export function getAccessToken() {
   return willExpireToken(accessToken) ? null : accessToken;
 }
 
-export function getRefreshToken() {
+export function getRefreshTokenApi() {
   const refreshToken = localStorage.getItem(REFRESH_TOKEN);
 
   if (!refreshToken || refreshToken === "null") {
     return null;
   }
   return willExpireToken(refreshToken) ? null : refreshToken;
+}
+
+export function refreshAccessToken(refreshToken) {
+  const url = `${basePath}/${apiVersion}/refresh-access-token`;
+  const bodyObj = {
+    refreshToken: refreshToken,
+  };
+  const params = {
+    method: "POST",
+    body: JSON.stringify(bodyObj),
+    headers: {
+      "vontent-Type": "application/json",
+    },
+  };
+  fetch(url, params)
+    .then((response) => {
+      if (response.status !== 200) {
+        return null;
+      }
+      return response.json();
+    })
+    .then((result) => {
+      if (!result) {
+        // TO DO: Desloguear usuario
+      } else {
+        const { accessToken, refreshToken } = result;
+        localStorage.setItem(ACCESS_TOKEN, accessToken);
+        localStorage.setItem(REFRESH_TOKEN, refreshToken);
+      }
+    });
 }
 
 function willExpireToken(token) {
